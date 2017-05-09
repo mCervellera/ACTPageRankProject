b0bbc9dc66e2400f8998407a1f10f0d7bb1faa8d
FrancescoMargiotta <francesco.margiotta.casaluci@gmail.com>
2017-05-09 17:35:04 +0200

    Correzioni aggiuntive

--- ConsensusDistributed.m
+++ ConsensusDistributed.m
@@ -1,24 +1,25 @@
-function [ x,x1 ] = ConsensusDistributed(M)
+function [x,e] = ConsensusDistributed(M,x_star)
 %La funzione ConsensusDistributed prende in input l'array di matrici.
-m=0.15;
-[r,c] = size(M); %serve a conoscere la dimensione della network
+[r,c] = size(M(:,:,1)); %serve a conoscere la dimensione della network
 %nel ciclo for, ad ogni istante k, devo richiamare la funzione che genera
-%la matrice distribuita ed usarla nel mio schema di update.
-i = randi([1 r],1,1); %generazione indice random per la scelta di A_i o M_i
+%la matrice distribuita ed usarla nel mio schema di update. 
 for k=1:1:c
 x(k) = 1/c;
 end
-for k=1:1:c
-x1(k) = 1/c;
+y = zeros(c,1);
+for k=1:1:1000 %calcolo update come def in paragrafo 3.2
+z=x';
+%generazione indice random per la scelta di A_i o M_i
+i = randi([1 r],1,1);
+x = (M(:,:,i)*z)';
+y = x' + y;
+stem(k,y(1)/k),hold on; %calcola la media parziale di y(k)
+ %mostriamo l'evoluzione del vettore fino al raggiungimento del consensus
 end
-for k=1:1:100 %calcolo update come def in paragrafo 3.2
-z=x;
-x = M(i)*z; 
-disp(x) %mostriamo l'evoluzione del vettore fino al raggiungimento del consensus
-end
-for k=1:1:100 %calcolo update come def in paragrafo 3
-z=x1;
-x1 = (1-m)* M(i)*z + (m/r)*ones(r);
-disp(x1) %mostriamo l'evoluzione del vettore fino al raggiungimento del consensus
-end
+y = y/k;
+e = sum((y -x_star).^2)/c;
+disp(y');
+%disp(e);
+err=sprintf('error %.8f', e);
+disp(err);
 end
