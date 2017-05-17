794e28d8c90b706c191c28c1e5e53cb2c9b8e5b4
FrancescoMargiotta <francesco.margiotta.casaluci@gmail.com>
2017-05-17 12:56:23 +0200

    Sistemati grafici
    Rimossi grafici dal for -> velocizzato plotting

--- PageRankCentralized.m
+++ PageRankCentralized.m
@@ -5,34 +5,40 @@ MAXITERATIONS= intmax('int64')-2; % il piu grande numero rappresentabile in Matl
 x = zeros(c,1);
 z = zeros(c,1);
 threshold=0.001;
+elem=0;
 
 for k=1:1:c
 x(k) = 1/c;
 end
 
-h1=figure('Name','Centralized: Estimation Error')
-h2=figure('Name','Centralized: Convergence to Consensus of the first 4 elements')
 for k=1:1:MAXITERATIONS
-    
+    elem= elem+1;
     z = x;
     x = M*z;
-    diff=norm(x-z);  
+    dif=norm(x-z);  
+    vet(k)=dif;
+    data(:,k) =x;
 
-    figure(h2)
-    scatter(k,x(1),'b'); hold on   % rappresentazione i primi 4 valori del PageRank
-    scatter(k,x(2),'g');
-    scatter(k,x(3),'r');
-    scatter(k,x(4),'k');    
-    legend('node1','node2','node3','node4')
-   
-    figure(h1)    
-    scatter(k,diff,'filled'); hold on % errore che tende a zero
-    
 
-    if(diff<=threshold)
+    if(dif<=threshold)
         break;
     end 
 end
+
+
+w= 1:1:elem;
+
+figure('Name','Centralized: Convergence to Consensus of the first 4 elements')
+scatter(w,data(1,w),'b'); hold on   % rappresentazione i primi 4 valori del PageRank
+scatter(w,data(2,w),'g');
+scatter(w,data(3,w),'r');
+scatter(w,data(4,w),'k');    
+legend('node1','node2','node3','node4')
+
+figure('Name','Centralized: Estimation Error') 
+scatter(w,vet,'filled'); hold on % errore che tende a zero
+
+
 disp(k)
 disp(x')
 
--- PageRankDistributed.m
+++ PageRankDistributed.m
@@ -6,56 +6,53 @@ function [x,e] = PageRankDistributed(M,x_star)
 
 MAXITERATIONS= intmax('int64')-2; % il piu grande numero rappresentabile in Matlab
 threshold=0.0035; % usare 0.0035
+elem=0;
 
 for k=1:1:c
 x(k) = 1/c;
 end
 y = zeros(c,1);
-h3=figure('Name','Distributed: Convergence to Consensus of the first 4 elements')
-h4=figure('Name','Distributed: Estimation Error')
+
 
 for k=1:1:MAXITERATIONS %calcolo update come def in paragrafo 3.2
-z=x';
-%generazione indice random per la scelta di A_i o M_i
-i = randi([1 r],1,1);
-x = (M(:,:,i)*x')';
-y = x' + y;
-diff=norm(x'-z);
-y_temp = y/double(k);
-e = sum((y_temp -x_star).^2)/c;
-norm1=norm(y_temp -x_star,1);
-norm_inf=norm(y_temp -x_star,inf);
-
-disp(diff)
-%mostriamo l'evoluzione del vettore fino al raggiungimento del consensus
-if(mod(k,30)==0)
-figure(h3)
-scatter(k,y(1)/double(k),'b'),hold on; %calcola la media parziale di y(k)
-scatter(k,y(2)/double(k),'g');
-scatter(k,y(3)/double(k),'r');
-scatter(k,y(4)/double(k),'k');    
-legend('node1','node2','node3','node4')
-
-end
-
-figure(h4)
-scatter(k,e,'b'); hold on 
-scatter(k,norm1,'g'); 
-scatter(k,norm_inf,'r'); 
-legend('mean square error','norm1','norm inf')
-
-
-  if(diff<=threshold)
+    elem = elem +1;
+    z=x';
+    %generazione indice random per la scelta di A_i o M_i
+    i = randi([1 r],1,1);
+    x = (M(:,:,i)*x')';
+    y = x' + y;
+    data(:,k) =y/double(k);
+    diff=norm(x'-z);
+    
+    y_temp = y/double(k);
+    e = sum((y_temp -x_star).^2)/c;
+    error(k) = e; 
+    norm1=norm(y_temp -x_star,1);
+    vect_norm1(k)= norm1;
+    norm_inf=norm(y_temp -x_star,inf);
+    vect_norm_inf(k) = norm_inf;
+   
+    if(diff<=threshold)
         break;
   end
 end
-disp(k)
-disp(y)
-y = y/double(k);
-e = sum((y -x_star).^2)/c;
+w= 1:1:elem;
+%mostriamo l'evoluzione del vettore fino al raggiungimento del consensus
+figure('Name','Distributed: Convergence to Consensus of the first 4 elements')
+scatter(w,data(1,w),'b'),hold on; %calcola la media parziale di y(k)
+scatter(w,data(2,w),'g');
+scatter(w,data(3,w),'r');
+scatter(w,data(4,w),'k');    
+legend('node1','node2','node3','node4')
 
+figure('Name','Distributed: Estimation Error')
+scatter(w,error,'b'); hold on 
+scatter(w,vect_norm1,'g'); 
+scatter(w,vect_norm_inf,'r');
+legend('mean square error','norm1','norm inf')
+% 
+% disp(e)
 
-disp(y');
 
 err=sprintf('error %.8f', e);
 disp(err);
