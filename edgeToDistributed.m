f7faca2487f9049350bba13aaebb8ac80dce3461
mCervellera <Lsmile1993@gmail.com>
2017-05-17 12:51:52 +0200

    New distributed matrix stuff
    oh yeeeah

--- edgeToDistributed.m
+++ edgeToDistributed.m
@@ -1,34 +1,20 @@
-function y = edgeToDistributed(x)
-%script che calcola la matrice di adiacenza per il consenso distribuito
-n = max(max(x));
+function y = edgeToDistributed( outLink, inLink, agentIndex , n )
 
-for i = 1:n
-    %inizializzazione di A
-    A(:,:,i) = zeros(n);
-    
-    %calcolo del subset di E interessato
-    %Estrazione indici dove è presente i nella prima colonna
-    E1 = find(x(:,1) == i);
-    %nella seconda colonna
-    E2 = find(x(:,2) == i);
-    %dove sono uguali
-    E3 = find(x(:,1) == x(:,2));
-    E = union(E1,E2);
-    E = union(E, E3);
-    
-    sizeOfE = size(E);
-    
-    Ei = x(E, :);
-    
-    %numero di agenti entranti in j da Ei
-    
-    for j = 1:sizeOfE
-        nij = size(find(Ei(:,2) == Ei(j,2)));
-        A(Ei(j,2),Ei(j,1),i) = 1/nij(1);
+Ad = eye(n);
+
+%compute column
+for i = 1:size(outLink)
+    Ad(outLink(i),agentIndex) = 1 / size(outLink,1);
+end
+%compute rows
+for i = 1:size(inLink)
+    Ad(agentIndex, inLink(i,1)) = 1 / inLink(i,2);
+    if i ~= agentIndex
+        Ad(inLink(i,1), inLink(i,1)) = 1 - 1 / inLink(i,2);
     end
-
+    Ad(agentIndex, agentIndex) = 0;
 end
-    
-y = A;
 
-end
+
+
+y = computeM(Ad);
