57de7724ec6cb54654a1b47102df89e81884ca59
Matteo <fuccula10@hotmail.it>
2017-05-26 13:28:27 +0200

    Marco: grafo undirected

--- randomEdgesGenerator.m
+++ randomEdgesGenerator.m
@@ -8,10 +8,10 @@ rng(seed);           % random seed
   while true
     Adj = binornd(1,0.2,NN,NN); % each entry is 1 with prob 0.2
 
-    Adj = or(Adj,Adj'); % symmetrize - undirected
+    %Adj = or(Adj,Adj'); % symmetrize - undirected
     I_NN = eye(NN);
     
-    %Adj = or(Adj,I_NN); % add self-loops
+    Adj = or(Adj,I_NN); % add self-loops
     
     testAdj = (I_NN+Adj)^NN; % check if G is connected
 
@@ -26,18 +26,22 @@ rng(seed);           % random seed
   edgesProgress = [];
   
   for i = 1:NN
-      for j = i:NN
+      for j = 1:NN
           if i==j || Adj(i,j) == 0
               continue;
           end
-          edgesProgress = [edgesProgress ; [i , j] ; [ j , i ]];
+          edgesProgress = [edgesProgress ; [i , j]]; %; [ randomReturn , i ]];
       end
       progress = ((i )/ (NN)) * 100;
           %disp(progress);
+    randomReturn = randi(NN, 1);
+    while ( randomReturn == i)
+        randomReturn = randi(NN, 1);
+    end
+    edgesProgress = [edgesProgress ; [ randomReturn , i ]];
   end
   
-  
-E = edgesProgress;
+E = unique(edgesProgress, 'rows')
 
 end
 
